289dbca7512a2bafebd134980140dabfcd09a8de
samin <84340577+HGSChandeepa@users.noreply.github.com>
2023-10-30 23:30:56 +0530

    drop down and data picker

--- lib/widgets/add_expence.dart
+++ lib/widgets/add_expence.dart
@@ -1,3 +1,4 @@
+import 'package:expence_tracker_app/models/expence.dart';
 import 'package:flutter/material.dart';
 
 class AddNewExpencce extends StatefulWidget {
@@ -10,6 +11,30 @@ class AddNewExpencce extends StatefulWidget {
 class _AddNewExpencceState extends State<AddNewExpencce> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+
+  DateTime _selectedDate = DateTime.now();
+  Category _selectedCategory = Category.travel;
+
+  final DateTime initialDate = DateTime.now();
+  final DateTime firstDate = DateTime(
+      DateTime.now().year - 1, DateTime.now().month, DateTime.now().day);
+  final DateTime lastDate = DateTime.now();
+  //opendate modal
+  Future<void> _openDateModal() async {
+    try {
+      final pickedDate = await showDatePicker(
+          context: context,
+          initialDate: initialDate,
+          firstDate: firstDate,
+          lastDate: lastDate);
+
+      setState(() {
+        _selectedDate = pickedDate!;
+      });
+    } catch (err) {
+      debugPrint(err.toString());
+    }
+  }
 
   //dispose methodes (this will clear the data form)
   @override
@@ -41,12 +66,26 @@ class _AddNewExpencceState extends State<AddNewExpencce> {
                 child: TextField(
                   controller: _amountController,
                   decoration: const InputDecoration(
-                    hintText: "Enter the aoumt",
-                    label: Text("Amount"),
-                  ),
+                      hintText: "Enter the aoumt",
+                      label: Text("Amount"),
+                      prefixText: "\$ "),
                   keyboardType: TextInputType.number,
                 ),
               ),
+
+              //date picker
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(formatedDate.format(_selectedDate)),
+                    IconButton(
+                        onPressed: _openDateModal,
+                        icon: const Icon(Icons.date_range_rounded))
+                  ],
+                ),
+              )
             ],
           ),
 
@@ -57,23 +96,49 @@ class _AddNewExpencceState extends State<AddNewExpencce> {
           ),
           Row(
             children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name,
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedCategory = value!;
+                  });
                 },
-                style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.redAccent),
-                ),
-                child: const Text("Close"),
               ),
               const Spacer(),
-              ElevatedButton(
-                onPressed: () {},
-                style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.green),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      style: const ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll(Colors.redAccent),
+                      ),
+                      child: const Text("Close"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: const ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(Colors.green),
+                      ),
+                      child: const Text("Save"),
+                    ),
+                  ],
                 ),
-                child: const Text("Save"),
-              ),
+              )
             ],
           )
         ],
